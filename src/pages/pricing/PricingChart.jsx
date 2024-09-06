a9a67e25634f8bd712b1ffdd54c8994ad4191f38
Md. Sajjad Hossain <153279113+sajjad-007@users.noreply.github.com>
2024-09-06 23:01:03 +0600

    pricing cart added

--- src/pages/pricing/PricingChart.jsx
+++ src/pages/pricing/PricingChart.jsx
@@ -19,8 +19,82 @@ const PricingChart = () => {
                 <h2 className='heading2 text-dark-blue'>our pricing part</h2>
                 <Peragraph className='pera w-[562px] mx-auto' text={pera1}/>
             </Div>
-            <Div className="cart_section mt-16">
-                <Div className="cart_item hover:bg-primary  w-[406px] h-auto py-16 pr-[77px] pl-[48px] bg-pera rounded-xl flex flex-col transition-all ease-linear duration-300">
+            <Div className="cart_section mt-16 flex flex-wrap gap-[31px]">
+                <Div className="cart_item w-[406px] h-auto py-16 pr-[77px] pl-[48px] bg-pera rounded-xl flex flex-col transition-all ease-linear duration-300">
+                    <Div className="head_section flex gap-2 items-center">
+                        <Span className='heading3 text-dark-blue group-hover:text-white' text='$299'/>
+                        <Peragraph className='Label1 capitalize' text='per design'/>
+                    </Div>
+                    <Div className="sub_section mt-5 ">
+                            <h6 className='heading6 mb-2'>landing page</h6>
+                            <Peragraph className='pera' text={pera2}/>
+                    </Div>
+                    <Div className="lists_section mt-20">
+                            <Ul className='flex flex-col gap-6'>
+                                <List className='flex gap-3 items-center'>
+                                    <IoLogoGooglePlaystore className='text-green' />
+                                    <Peragraph className='Label1 text-dark-blue' text='All limited links'/>
+                                </List>
+                                <List className='flex gap-3 items-center'>
+                                    <IoLogoGooglePlaystore className='text-green' />
+                                    <Peragraph className='Label1 text-dark-blue' text='All limited links'/>
+                                </List>
+                                <List className='flex gap-3 items-center'>
+                                    <IoLogoGooglePlaystore className='text-green' />
+                                    <Peragraph className='Label1 text-dark-blue' text='All limited links'/>
+                                </List>
+                                <List className='flex gap-3 items-center'>
+                                    <IoLogoGooglePlaystore className='text-green' />
+                                    <Peragraph className='Label1 text-dark-blue' text='All limited links'/>
+                                </List>
+                                <List className='flex gap-3 items-center'>
+                                    <IoLogoGooglePlaystore className='text-green' />
+                                    <Peragraph className='Label1 text-dark-blue' text='All limited links'/>
+                                </List>
+                            </Ul>   
+                    </Div>
+                    <Div className="btn mt-20 text-center">
+                            <Button className='nav_btn3' type='submit' text='get started'/>
+                    </Div>
+                </Div>
+                <Div className="cart_item w-[406px] h-auto py-16 pr-[77px] pl-[48px] bg-pera rounded-xl flex flex-col transition-all ease-linear duration-300">
+                    <Div className="head_section flex gap-2 items-center">
+                        <Span className='heading3 text-dark-blue group-hover:text-white' text='$299'/>
+                        <Peragraph className='Label1 capitalize' text='per design'/>
+                    </Div>
+                    <Div className="sub_section mt-5 ">
+                            <h6 className='heading6 mb-2'>landing page</h6>
+                            <Peragraph className='pera' text={pera2}/>
+                    </Div>
+                    <Div className="lists_section mt-20">
+                            <Ul className='flex flex-col gap-6'>
+                                <List className='flex gap-3 items-center'>
+                                    <IoLogoGooglePlaystore className='text-green' />
+                                    <Peragraph className='Label1 text-dark-blue' text='All limited links'/>
+                                </List>
+                                <List className='flex gap-3 items-center'>
+                                    <IoLogoGooglePlaystore className='text-green' />
+                                    <Peragraph className='Label1 text-dark-blue' text='All limited links'/>
+                                </List>
+                                <List className='flex gap-3 items-center'>
+                                    <IoLogoGooglePlaystore className='text-green' />
+                                    <Peragraph className='Label1 text-dark-blue' text='All limited links'/>
+                                </List>
+                                <List className='flex gap-3 items-center'>
+                                    <IoLogoGooglePlaystore className='text-green' />
+                                    <Peragraph className='Label1 text-dark-blue' text='All limited links'/>
+                                </List>
+                                <List className='flex gap-3 items-center'>
+                                    <IoLogoGooglePlaystore className='text-green' />
+                                    <Peragraph className='Label1 text-dark-blue' text='All limited links'/>
+                                </List>
+                            </Ul>   
+                    </Div>
+                    <Div className="btn mt-20 text-center">
+                            <Button className='nav_btn3' type='submit' text='get started'/>
+                    </Div>
+                </Div>
+                <Div className="cart_item w-[406px] h-auto py-16 pr-[77px] pl-[48px] bg-pera rounded-xl flex flex-col transition-all ease-linear duration-300">
                     <Div className="head_section flex gap-2 items-center">
                         <Span className='heading3 text-dark-blue group-hover:text-white' text='$299'/>
                         <Peragraph className='Label1 capitalize' text='per design'/>
